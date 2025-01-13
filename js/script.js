3645a50f0083e4578aba1fd8984293e853b8cfde
Siddharthv97 <58696356+Siddharthv97@users.noreply.github.com>
2025-01-13 12:04:46 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const snakeBody = document.getElementById("snake_body");
+const snakeHead = document.getElementById("snake_head");
+const snakeTail = document.getElementById("snake_tail");
+const playGround = document.querySelector(".playground");
+
+
+
+// Game Starting 
+document.addEventListener("keydown", ()=> {
+    const menu = document.getElementById("menu");
+    menu.style.display = "none";
+});
+
+
